20cf5e849cf64ce8487ded3ce609dfe6bf82f509
Sanjeet <thereallycoolstuff100@gmail.com>
2017-05-02 13:21:26 +0100

    Update templateModule.py

--- modules/templateModule.py
+++ modules/templateModule.py
@@ -2,6 +2,6 @@ moduleName = "templateModule"
 commandWords = ["xxx"]
 
 def execute(command):
-    #Write anything you want to be execute when the commandWords are heard
+    #Write anything you want to be executed when the commandWords are heard
     #The 'command' parameter is the command you speak
     return
